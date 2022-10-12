1b60bb068bdf5223170dc26a87e019282e89d553
Maksden567 <babadid346@gmail.com>
2022-10-12 20:51:20 +0300

    js

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,1 +1,31 @@
-console.log('components');
+
+const burgerBtn=document.querySelector('.burger__img')
+const closeBtn=document.querySelector('.close__btn')
+const list=document.querySelector('.list')
+
+
+const plays=document.querySelectorAll('.play')
+
+
+
+burgerBtn.addEventListener('click',function(){
+    list.style.top=0;
+})
+closeBtn.addEventListener('click',function(){
+   list.style.top="-216px";
+})
+
+
+plays.forEach(item=>{
+    item.addEventListener('click',function(){
+        item.style.display="none"
+        item.previousElementSibling.play()
+        item.previousElementSibling.controls=true
+       
+    }) 
+    item.previousElementSibling.addEventListener('ended',function(){
+            item.previousElementSibling.load()
+            item.style.display="block"
+            item.previousElementSibling.controls=false
+        })
+})
